64a192eb655d813af39d43e4814ece65a3ad6f0d
Michelle <56207268+MichelleGray78@users.noreply.github.com>
2021-12-30 10:45:58 +0000

    gradeBookProject - First Commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+// String name = "Michelle";
+int age = 21;
+if(args.Length > 0){
+Console.WriteLine($"Hello, I am {args[0]}\n\t and I\"m {age} years old");
+}
+else {
+    Console.WriteLine("No Argument was passed in");
+}
